917d15b6a947bdd74b1bfaff682f42157c703fda
NotAShelf <raf@notashelf.dev>
2025-07-29 17:09:46 +0300

    modules/nixos: set XDG variables while nixdg-ninja is enabled
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a69640b6a3c4e69e51e14c850e08b6e2c65ed

--- modules/nixos/module.nix
+++ modules/nixos/module.nix
@@ -3,27 +3,39 @@
   lib,
   ...
 }: let
+  inherit (lib.modules) mkIf;
+
   cfg = config.programs.nixdg-ninja;
 in {
   imports = [
     ../options.nix
   ];
 
-  config.environment = let
-    enabledPrograms = lib.filterAttrs (_: v: v.enable) cfg.programs;
+  config = mkIf cfg.enable {
+    environment = let
+      enabledPrograms = lib.filterAttrs (_: v: v.enable) cfg.programs;
 
-    allFilesByTarget =
-      lib.concatMapAttrs (
-        _: prog:
-          lib.mapAttrs (_: file: {inherit (file) text;})
-          (lib.filterAttrs (_: file: file.enable && file.text != null) prog.files)
-      )
-      enabledPrograms;
-  in {
-    # Set up global variables from collected variables
-    variables = cfg.env.globalVars;
+      allFilesByTarget =
+        lib.concatMapAttrs (
+          _: prog:
+            lib.mapAttrs (_: file: {inherit (file) text;})
+            (lib.filterAttrs (_: file: file.enable && file.text != null) prog.files)
+        )
+        enabledPrograms;
+    in {
+      # Set up global variables from collected variables
+      variables = cfg.env.globalVars;
 
-    # Link files associated with enabled programs
-    etc = allFilesByTarget;
+      sessionVariables = {
+        XDG_CACHE_HOME = "\${HOME}/.cache";
+        XDG_CONFIG_HOME = "\${HOME}/.config";
+        XDG_DATA_HOME = "\${HOME}/.local/share";
+        XDG_STATE_HOME = "\${HOME}/.local/state";
+        XDG_BIN_HOME = "\${HOME}/.local/bin";
+      };
+
+      # Link files associated with enabled programs
+      etc = allFilesByTarget;
+    };
   };
 }
